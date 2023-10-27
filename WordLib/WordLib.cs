39c909cb0ab8fea3a52b52e6bb384d92b745b72c
Morgan Lindzén <114500918+Lindzen-Morgan@users.noreply.github.com>
2023-10-27 15:56:05 +0200

    Add files via upload

---
+++ WordLib/WordLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WordLib
+{
+    public class WordChecker
+    {
+        public static bool IsPalindrome(string word)
+        {
+            // Check if the word is a palindrome
+            string reversed = new string(word.Reverse().ToArray());
+            return word == reversed;
+        }
+
+    }
+}
